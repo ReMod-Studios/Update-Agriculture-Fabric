c73b891f6cce1aaae63c702ab940fc42de200ee9
LorenzoPapi <42911751+LorenzoPapi@users.noreply.github.com>
2021-01-13 16:57:25 +0100

    Now the pail waters the farmland

--- src/main/java/com/remodstudios/updagri/item/WateringPailItem.java
+++ src/main/java/com/remodstudios/updagri/item/WateringPailItem.java
@@ -2,6 +2,9 @@ package com.remodstudios.updagri.item;
 
 import com.remodstudios.updagri.Updagri;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FarmlandBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -9,6 +12,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class WateringPailItem extends Item {
@@ -18,14 +22,28 @@ public class WateringPailItem extends Item {
 	}
 
 	@Override
+	public void onCraft(ItemStack stack, World world, PlayerEntity player) {
+		CompoundTag tag = stack.getOrCreateTag();
+		tag.putBoolean("hasWater", false);
+	}
+
+	@Override
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-		CompoundTag tag = user.getStackInHand(hand).getOrCreateTag();
-		if (!tag.contains("hasWater")) {
-			tag.putBoolean("hasWater", false);
+		if (!world.isClient()) {
+			CompoundTag tag = user.getStackInHand(hand).getOrCreateTag();
+			if (tag.getBoolean("hasWater")) {
+				BlockPos playerPos = user.getBlockPos().down();
+				Iterable<BlockPos> positions = BlockPos.iterate(playerPos.east().north(), playerPos.west().south());
+				//TODO: keep it like this for 300 ticks (might need mixin tbh)
+				positions.forEach((pos) -> {
+					if (world.getBlockState(pos).getBlock() == Blocks.FARMLAND && world.getBlockState(pos).get(FarmlandBlock.MOISTURE) < 7) {
+						world.setBlockState(pos, world.getBlockState(pos).with(FarmlandBlock.MOISTURE, 7));
+					}
+				});
+				return TypedActionResult.success(user.getStackInHand(hand));
+			}
+			return TypedActionResult.pass(user.getStackInHand(hand));
 		}
-		if (tag.getBoolean("hasWater")) {
-			Updagri.LOGGER.info("hasWater");
-		}
-		return TypedActionResult.pass(user.getStackInHand(hand));
+		return TypedActionResult.fail(user.getStackInHand(hand));
 	}
 }
